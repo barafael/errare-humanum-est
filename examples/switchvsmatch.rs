27b7bf955ebe02fdcc7d448a3dc3a7e67e458885
Rafael Bachmann <mediumendian@gmail.com>
2018-06-15 16:58:02 +0200

    match block fancify

--- examples/switchvsmatch.rs
+++ examples/switchvsmatch.rs
@@ -2,18 +2,19 @@ use std::io::{self, BufRead};
 
 fn main() {
     loop {
-        println!("Enter an integer number from 1 to 20 inclusive\n");
+        println!("Enter an integer number:");
 
         let stdin = io::stdin();
         let buffer = stdin.lock().lines().next().unwrap().unwrap();
 
         let choice: usize = buffer.parse().unwrap_or(100);
-
-        match choice {
-            0..=5 => println!("Really low number"),
-            6..=15 => println!("Thats better"),
-            16..=20 => println!("Up there"),
-            _ => println!("I said 0 to 20 inclusive!"),
-        }
+        let result = match choice {
+            0 => "zero for u",
+            1...9 => "choice not great",
+            i if i % 2 == 1 => "larger than or equal 11, and odd",
+            i if i == 300 => "This is Sparta!",
+            _ => "larger than or equal 10, and even",
+        };
+        println!("{}", result);
     }
 }
