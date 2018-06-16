46bac3c9d6d208108cdc8f99cbf02ac5e57e447f
Rafael Bachmann <mediumendian@gmail.com>
2018-06-16 09:59:48 +0200

    use stack arrays in array bounds example

--- examples/array_bounds.c
+++ examples/array_bounds.c
@@ -1,15 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define ARRAY_SIZE 15
-#define OVERRUN_BYTES 10
+#include<stdio.h>
 
 int main() {
-    int *array = malloc(ARRAY_SIZE * sizeof(int));
+    int array[] = {1, 2, 3, 4, 5};
 
-    /* Valgrind detects invalid write, but program worked as if array was larger */
-    for (int index = 0; index < ARRAY_SIZE + OVERRUN_BYTES; index++) {
+    for (int index = 0; index < 5 + 5; index++) {
         array[index] = 3;
         printf("%d ", array[index]);
     }
--- examples/array_bounds.rs
+++ examples/array_bounds.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let mut array: Box<Vec<u32>> = Box::new(vec![0; 15]);
+    let mut array = [1, 2, 3, 4, 5];
 
     for i in 0..array.len() + 5 {
         array[i] = 3;
